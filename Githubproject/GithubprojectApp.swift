90b77ce4cd51d9c30a57582acbd6033e0b5cff54
Eden Brown <3elbrown46@gmail.com>
2023-07-13 13:18:46 -0500

    Initial Commit

---
+++ Githubproject/GithubprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubprojectApp.swift
+//  Githubproject
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
