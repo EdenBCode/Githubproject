0dfbbf973601c78d0c8d5b0b82e3b9695b5ceaf9
Eden Brown <3elbrown46@gmail.com>
2023-07-13 13:36:31 -0500

    1 change

--- Githubproject/ContentView.swift
+++ Githubproject/ContentView.swift
@@ -1,4 +1,4 @@
-//
+   //
 //  ContentView.swift
 //  Githubproject
 //
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+           
+            Text("Bye!")
         }
         .padding()
     }
